8c0fb6d28b54e1ed4a074829e61af56b30dd2a0d
gabas-guedes <gabrielsamavi15@gmail.com>
2023-09-05 11:05:37 -0300

    ss

---
+++ Atividade cédulas.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+
+    int A,B; 
+
+    scanf("%d%d",A,B);
+
+   X = A+B;
+
+    printf("X = %d",X);
+    
+    return 0;
+}
---
+++ Atividade Área do Círculo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+
+    int A,B; 
+
+    scanf("%d %d",&A,&B);
+
+    int X = A+B;
+
+    printf("X = %d",X);
+    
+    return 0;
+}
