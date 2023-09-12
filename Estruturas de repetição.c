d8fb829274426dd8dd383bb0360e1e65baa8d3d7
gabas-guedes <gabrielsamavi15@gmail.com>
2023-09-12 10:05:54 -0300

    new update

--- Estruturas de repetição.c
+++ Estruturas de repetição.c
@@ -3,14 +3,14 @@
 int main(){
 
 
-    for(i = 0; i<= 2; i++){
-        printf("valor de i: %d!!!!\n", i);
+    for(í = 0; í<= 2; i++){
+        printf("valor de í: %d!!!!\n", í);
 
     }
 
 
 
-
+    print("saiu do for. valor de í: %d\n" í);
 
     return 0;
 }
